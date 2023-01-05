bef7c1a91d6eed7ae0f5857b7f4f93fd80405891
Jon V. Apiscope <Jon.Apiscope@credicard.com.ve>
2023-01-05 09:35:36 -0400

    commit trabajo

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -23,7 +23,7 @@
         <div class="hidden sm:ml-6 sm:block">
           <div class="flex space-x-4">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="{{route('admin.home')}}" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</a>
+            <a href="{{route('admin.home')}}" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard jeje</a>
 
             @foreach ($categories as $category)
                 
